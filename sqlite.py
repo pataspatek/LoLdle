39bedb5f9844ed06a88374952578a0efcf68088b
pataspatek <patrikpatek3@gmail.com>
2023-02-14 10:58:20 +0100

    sqlite3 database

---
+++ sqlite.py
@@ -0,0 +1,18 @@
+import sqlite3
+
+
+# Create a connection to the database
+conn = sqlite3.connect('loldle.db')
+
+# Create a cursor object to execute SQL queries
+cursor = conn.cursor()
+
+def main():
+    pass
+
+
+
+if __name__ == '__main__':
+    main()
+
+
